cfeab99feea39b5798a68b6b07a4045249a6ff13
Arief Putra <sibuearief@gmail.com>
2026-03-06 08:20:15 +0700

    refactor: remove unused function

--- pkg/timezone/indonesia.go
+++ pkg/timezone/indonesia.go
@@ -4,17 +4,12 @@ import "time"
 
 // Indonesian timezone offsets
 const (
-	WIB  = "+07:00" //  Wester is UTC+7
-	WITA = "+08:00" //  Central is UTC+8
-	WIT  = "+09:00" //  Eastern is UTC+9
+	WIB  = "+07:00" //  Western Indonesian Time is UTC+7
+	WITA = "+08:00" //  Central Indonesian Time is UTC+8
+	WIT  = "+09:00" //  Eastern Indonesian Time is UTC+9
 )
 
-// Location returns the time.Location for a given timezone name
-func Location(name string) (*time.Location, error) {
+// LocationByName returns the time.Location for a given timezone name
+func LocationByName(name string) (*time.Location, error) {
 	return time.LoadLocation(name)
-}
-
-// OffsetDuration returns the time.Duration for a timezone offset string
-func OffsetDuration(offset string) (time.Duration, error) {
-	return time.ParseDuration(offset)
 }
