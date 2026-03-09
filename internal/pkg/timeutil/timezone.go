31797df2d9faf05c80c7d0bfa6bdc7ec8c83d951
Arief Putra <sibuearief@gmail.com>
2026-03-09 17:49:51 +0700

    refactor: remove unused file

--- internal/pkg/timeutil/timezone.go
+++
@@ -1,8 +0,0 @@
-package timeutil
-
-// Indonesian timezone offsets
-const (
-	WIB  = "+07:00" //  Western Indonesian Time is UTC+7
-	WITA = "+08:00" //  Central Indonesian Time is UTC+8
-	WIT  = "+09:00" //  Eastern Indonesian Time is UTC+9
-)
